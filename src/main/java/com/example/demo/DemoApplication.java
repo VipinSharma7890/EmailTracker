911d779e6ffcd5c85952393bcbebcb49324a0019
VipinSharma7890 <118989768+VipinSharma7890@users.noreply.github.com>
2025-03-17 21:05:09 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication{
 	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
