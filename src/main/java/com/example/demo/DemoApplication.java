e20ab33928ef432d034896c7203791f1e7910f3d
VipinSharma7890 <118989768+VipinSharma7890@users.noreply.github.com>
2025-03-18 14:03:39 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication{
-	public String PORT = System.getenv("PORT");
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+import java.util.TimeZone;
 
+@SpringBootApplication
+public class EmailApp {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata")); // Set JVM timezone
+        SpringApplication.run(EmailApp.class, args);
+    }
 }
