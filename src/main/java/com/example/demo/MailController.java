89beca98a11bca9cf73f056ab2f4722311a9b23f
VipinSharma7890 <118989768+VipinSharma7890@users.noreply.github.com>
2025-03-20 20:12:32 +0530

    Update MailController.java

--- src/main/java/com/example/demo/MailController.java
+++ src/main/java/com/example/demo/MailController.java
@@ -23,7 +23,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
